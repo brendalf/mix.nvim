88a330cac038b8bf8c338739e06321d23a9aecd1
Breno Silva <brenophp@gmail.com>
2022-08-21 19:36:32 -0300

    [config] validate config returns boolean

--- lua/mix/config.lua
+++ lua/mix/config.lua
@@ -11,16 +11,22 @@ M.possible_values = {
 
 function M.validate_config(config)
     if not utils.has_value(M.possible_values.window, config.window) then
-        error(table.concat({ "opt window = '", config.window,
-            "' is not valid. valid options are: ", table.concat(M.possible_values.window, " | ") }))
+        local message = { "opt window = '", config.window,
+            "' is not valid. valid options are: ", table.concat(M.possible_values.window, " | ") }
+        vim.notify(table.concat(message), vim.log.levels.ERROR)
+        return false
     end
+
+    return true
 end
 
 function M.get_config(opts)
     local config = opts or {}
     config = vim.tbl_extend("force", M.defaults, config)
 
-    M.validate_config(config)
+    if not M.validate_config(config) then
+        error("invalid configuration, check :messages.")
+    end
 
     return config
 end
